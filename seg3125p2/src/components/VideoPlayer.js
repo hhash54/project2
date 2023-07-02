000ed2139b3b3d0979aed202c0f3ea0d4d54e341
hhash54 <hhashem2002@gmail.com>
2023-07-02 18:47:07 -0400

    kk

--- seg3125p2/src/components/VideoPlayer.js
+++ seg3125p2/src/components/VideoPlayer.js
@@ -1,11 +1,15 @@
 // src/components/VideoPlayer.js
 import React from 'react';
+import ReactPlayer from 'react-player';
 
 function VideoPlayer() {
   return (
-    <div>
-      // VideoPlayer content here
-    </div>
+    <ReactPlayer
+      url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" // replace with actual video URL
+      controls
+      width="100%"
+      height="100%"
+    />
   );
 }
 
